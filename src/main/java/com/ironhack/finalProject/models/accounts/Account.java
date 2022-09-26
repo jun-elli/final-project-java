54ed37b14f8b936102a01ba0b2056b2b48058f01
Jun <jun279r@gmail.com>
2022-09-26 15:15:29 +0200

    Build account as superclass

--- src/main/java/com/ironhack/finalProject/models/accounts/Account.java
+++ src/main/java/com/ironhack/finalProject/models/accounts/Account.java
@@ -2,13 +2,108 @@ package com.ironhack.finalProject.models.accounts;
 
 import com.ironhack.finalProject.models.Money;
 import com.ironhack.finalProject.models.users.AccountHolder;
+import org.springframework.lang.Nullable;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-
-public abstract class Account {
+@MappedSuperclass
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private Money balance;
+    @Transient
+    private Money money;
+    @NotNull
+    private BigDecimal balance;
+    @NotNull
+    private String currency;
+    @ManyToOne
+    @JoinColumn(name = "primary_owner_id", referencedColumnName = "id", nullable = false)
     private AccountHolder primaryOwner;
+    @Nullable
+    @ManyToOne
+    @JoinColumn(name = "secondary_owner_id", referencedColumnName = "id")
     private AccountHolder secondaryOwner;
     private final BigDecimal PENALTY_FEE = BigDecimal.valueOf(40);
+
+    public Account() {
+    }
+
+    public Account(Money money, AccountHolder primaryOwner, @Nullable AccountHolder secondaryOwner) {
+        this.money = money;
+        this.balance = money.getAmount();
+        this.currency = money.getCurrency().toString();
+        this.primaryOwner = primaryOwner;
+        this.secondaryOwner = secondaryOwner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
+    public void setMoney(Money money) {
+        if (this.currency == money.getCurrency().toString()){
+            this.money = money;
+            setBalance(money.getAmount());
+        }else{
+            throw new IllegalArgumentException("Currencies are different.");
+        }
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public AccountHolder getPrimaryOwner() {
+        return primaryOwner;
+    }
+
+    public void setPrimaryOwner(AccountHolder primaryOwner) {
+        this.primaryOwner = primaryOwner;
+    }
+
+    @Nullable
+    public AccountHolder getSecondaryOwner() {
+        return secondaryOwner;
+    }
+
+    public void setSecondaryOwner(@Nullable AccountHolder secondaryOwner) {
+        this.secondaryOwner = secondaryOwner;
+    }
+
+    public BigDecimal getPENALTY_FEE() {
+        return PENALTY_FEE;
+    }
+    public String printActualBalance(){
+        return this.money.toString();
+    }
+    //Basic add and substract money methods,
+    // they will be overridden to account for limits on subclasses
+    public void addMoney(Money addingMoney){
+        this.money.increaseAmount(addingMoney);
+    }
+    public void substractMoney(Money substractingMoney){
+        this.money.decreaseAmount(substractingMoney);
+    }
 }
