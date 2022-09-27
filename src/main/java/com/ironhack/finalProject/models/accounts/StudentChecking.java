985f5ed846f622e2a0ccc60d4db6d9ed1b955db9
Jun <jun279r@gmail.com>
2022-09-27 13:45:47 +0200

    Build entity student checking

--- src/main/java/com/ironhack/finalProject/models/accounts/StudentChecking.java
+++ src/main/java/com/ironhack/finalProject/models/accounts/StudentChecking.java
@@ -1,9 +1,56 @@
 package com.ironhack.finalProject.models.accounts;
 
+import com.ironhack.finalProject.models.Money;
+import com.ironhack.finalProject.models.enums.AccountStatus;
+import com.ironhack.finalProject.models.users.AccountHolder;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+@Entity
 public class StudentChecking extends Account{
 
-//    A secretKey
-//    A creationDate
-//    A status (FROZEN, ACTIVE)
+    @NotNull
+    private String secretKey;
+    @NotNull
+    private Date creationDate;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status = AccountStatus.DISABLED;
 
+    public StudentChecking() {
+    }
+
+    public StudentChecking(Money money, AccountHolder primaryOwner, AccountHolder secondaryOwner, String secretKey, Date creationDate, AccountStatus status) {
+        super(money, primaryOwner, secondaryOwner);
+        this.secretKey = secretKey;
+        this.creationDate = creationDate;
+        this.status = status;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
 }
