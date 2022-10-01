b4b7db67a0a4ae1b74a34efef05ac85c25e6dc17
Jun <jun279r@gmail.com>
2022-10-01 11:11:47 +0200

    Fix Role generated value to auto increment

--- src/main/java/com/ironhack/finalProject/config/security/Role.java
+++ src/main/java/com/ironhack/finalProject/config/security/Role.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Table(name = "roles")
 public class Role {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
